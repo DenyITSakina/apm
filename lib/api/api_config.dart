94d42177cafc3d1262381769ab1ebca65f228c31
fannyITsakina <sakinaidaman.programer8@gmail.com>
2026-06-24 15:14:56 +0700

    -

--- lib/api/api_config.dart
+++ lib/api/api_config.dart
@@ -27,11 +27,7 @@ class ApiConfig {
 
   static const String bpjsCekRujukan = '$baseUrl/bpjs/cek-rujukan';
 
-  static const String bookingUpdateStatus = '$baseUrl/booking/update-status';
-
   static const String getDaftar = '$baseUrl/apm-get-sosial';
-
-  static String bookingPasienBaru(int jenis) => '$baseUrl/pasien-baru/$jenis';
 }
 
 class ApiResponse<T> {
